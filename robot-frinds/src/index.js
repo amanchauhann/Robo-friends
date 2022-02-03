a7158320b74cbded585d6dea9a9106c5ded82546
Aman Chauhan <amangotpro@gmail.com>
2022-02-03 20:03:45 +0530

    Update

--- robot-frinds/src/index.js
+++ robot-frinds/src/index.js
@@ -2,13 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 // import App from './App';
-import Second from './Second';
+// import Second from './Second';
+import Card from './Card';
+import { RobotList } from './RobotList';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Second greeting={'hello' + 'my name is aman'} />
-    <link rel="stylesheet" href="https://npmcdn.com/tachyons@4.0.0-beta.35/css/tachyons.min.css"></link>
+    <Card id={RobotList[0].id} name={RobotList[0].name} email={RobotList[0].email} />
+    <Card id={RobotList[1].id} name={RobotList[1].name} email={RobotList[1].email} />
+    <Card id={RobotList[2].id} name={RobotList[2].name} email={RobotList[2].email} />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/tachyons/4.11.1/tachyons.min.css" integrity="sha512-d0v474klOFSF7qD9WDvyRxAvXaWSxCHDZdnBSZQjo8BpVr6vpjwAgqetpqkKP38DzlOzdVPaLVnzzW1Ba8wB9w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 
   </React.StrictMode>,
   document.getElementById('root')
