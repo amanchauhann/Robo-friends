9ac43cc6955be17f943e702a94b162822d52e019
Aman Chauhan <amangotpro@gmail.com>
2022-02-05 18:14:12 +0530

    Style: changed scroll

--- robot-frinds/src/Components/Scroll.js
+++ robot-frinds/src/Components/Scroll.js
@@ -1,9 +1,8 @@
 const Scroll = (props) => {
     return (
-        <div style={{ overflow: 'scroll', border: '5px solid black', height: '800px'}}>
+        <div style={{ overflowY: 'scroll', border: '5px solid black', height: '800px'}}>
             {props.children}
         </div>
     );
 }
-
 export default Scroll;
