465c5633d946bb26222d6b3891f87ae0d918314f
Aman Chauhan <amangotpro@gmail.com>
2022-02-05 11:47:53 +0530

    Style: SearchBox

--- robot-frinds/src/Searchbox.js
+++ robot-frinds/src/Searchbox.js
@@ -1,7 +1,7 @@
 const Searchbox =({searchchange}) =>{
     return (
         <div>
-            <input type='search' placeholder='Search for Robots' className="bg-lightest-blue" onChange={searchchange}></input>
+            <input type='search' placeholder='Search for Robots' className="bg-lightest-blue pd2 ma2" onChange={searchchange}></input>
         </div>
     )
 }
